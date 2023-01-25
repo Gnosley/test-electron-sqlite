61a4eb1ab5b5460ca7b27f6f0630f8be83551357
Jesse Francis <git@jessef.xyz>
2023-01-25 13:00:31 -0700

    switched sqlite pkg

--- src/api/counter.ts
+++ src/api/counter.ts
@@ -1,79 +1,42 @@
 import { db } from "./database";
 const COUNTER_ID = "0";
 
-const ensureTable = () =>
-  db.run("CREATE TABLE IF NOT EXISTS counter (id TEXT, count INT)");
+const ensureTable = db.exec(
+  "CREATE TABLE IF NOT EXISTS counter (id TEXT, count INT)"
+);
 
-const ensureCounter = () =>
-  db.run(
-    "INSERT INTO counter(id, count) SELECT ?, 0 WHERE NOT EXISTS (SELECT 1 FROM counter)",
-    COUNTER_ID
-  );
+const ensureCounter = db.prepare(
+  "INSERT INTO counter(id, count) SELECT ?, 0 WHERE NOT EXISTS (SELECT 1 FROM counter)"
+);
+
+const currentStmt = db.prepare(`SELECT count from counter WHERE id = ?`);
 
 export function current() {
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      ensureTable();
-      ensureCounter();
-      db.get(
-        `SELECT count from counter WHERE id = ?`,
-        COUNTER_ID,
-        (err, row) => {
-          if (err) reject(err);
-          resolve(row.count);
-        }
-      );
-    });
-  });
+  ensureCounter.run(COUNTER_ID);
+  return currentStmt.get(COUNTER_ID).count;
 }
 
-export function increment(): Promise<number> {
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      ensureTable();
-      ensureCounter();
-      db.get(
-        `UPDATE counter SET count = count + 1 WHERE id = ? RETURNING count`,
-        COUNTER_ID,
-        (err, row) => {
-          if (err) reject(err);
-          resolve(row.count);
-        }
-      );
-    });
-  });
+const incrementStmt = db.prepare(
+  `UPDATE counter SET count = count + 1 WHERE id = ? RETURNING count`
+);
+export function increment() {
+  ensureCounter.run(COUNTER_ID);
+  return incrementStmt.get(COUNTER_ID).count;
 }
 
+const decrementStmt = db.prepare(
+  `UPDATE counter SET count = count - 1 WHERE id = ? RETURNING count`
+);
 export function decrement() {
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      ensureTable();
-      ensureCounter();
-      db.get(
-        `UPDATE counter SET count = count - 1 WHERE id = ? RETURNING count`,
-        COUNTER_ID,
-        (err, row) => {
-          if (err) reject(err);
-          resolve(row.count);
-        }
-      );
-    });
-  });
+  ensureCounter.run(COUNTER_ID);
+  return decrementStmt.get(COUNTER_ID).count;
 }
+
+const resetStmt = db.prepare(
+  `UPDATE counter SET count = 0 WHERE id = ? RETURNING count`
+);
 
 export function reset() {
-  return new Promise((resolve, reject) => {
-    db.serialize(() => {
-      ensureTable();
-      ensureCounter();
-      db.get(
-        `UPDATE counter SET count = 0 WHERE id = ? RETURNING count`,
-        COUNTER_ID,
-        (err, row) => {
-          if (err) reject(err);
-          resolve(row.count);
-        }
-      );
-    });
-  });
+  ensureCounter.run(COUNTER_ID);
+  return resetStmt.get(COUNTER_ID).count;
 }
--- src/api/database.ts
+++ src/api/database.ts
@@ -1,4 +1,6 @@
-import { Database } from "sqlite3";
-const DATABASE_NAME = "test";
+import Database from "better-sqlite3";
+const DATABASE_NAME = "test.db";
+// const db = new Database(DATABASE_NAME);
 const db = new Database(DATABASE_NAME);
+db.pragma("journal_mode = WAL");
 export { db, DATABASE_NAME };
